432b2bd13a46981f5d0b86f478036982b65e7fee
Dean <72749793+drossano@users.noreply.github.com>
2022-02-10 17:57:58 -0500

    Fix append method and and prepend method

--- lib/linked-list.rb
+++ lib/linked-list.rb
@@ -1,10 +1,25 @@
 require_relative 'node'
 
 class LinkedList
-
+  attr_accessor :head, :tail
   def append(value)
-    node = Node.new
-    node.value(value)
-    node.next_node
+    node = Node.new(value)
+    @tail = node
+    if defined?(@head)
+      @tail.next_node = node
+    else
+      @head = node
+    end
   end
+
+  def prepend(value)
+    node = Node.new(value)
+    @head = node
+    if defined?(@head)
+      node.next_node = @head
+    else
+      @tail = node
+    end
+  end
+
 end
