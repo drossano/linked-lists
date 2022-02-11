ad605792b8d347b7a5d917e732640a3560f1e2dc
Dean <72749793+drossano@users.noreply.github.com>
2022-02-10 19:44:03 -0500

    Build pop method

--- lib/linked-list.rb
+++ lib/linked-list.rb
@@ -51,12 +51,31 @@ class LinkedList
         current_index += 1
         current_node = current_node.next_node
       end
+    else
+      nil
     end
     if current_node.nil?
       nil
     else
-      current_node.value
+      current_node
     end
+  end
+
+  def pop
+    if defined?(@head)
+      tail_index = 0
+      if @head.next_node.nil?
+        @head = nil
+        @tail = nil
+      else
+        until at(tail_index).nil?
+          tail_index += 1
+        end
+          @tail = at(tail_index - 2)
+          @tail.next_node = nil
+      end
+    end
+    nil
   end
 end
 
